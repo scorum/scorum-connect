6b00b6d3a53ad2102cbc0f4bab1c0d4bf46a1bee
Julien Bernier-Haineault <Jbernier-Haineault@c2.local>
2019-06-27 16:25:49 -0400

    Injector for username selection

--- app/ui/components/choose-account/choose-account.jsx
+++ app/ui/components/choose-account/choose-account.jsx
@@ -13,6 +13,11 @@ const ChooseAccountUI = ({ accounts, trx, notificationShowed, hideNotification, 
   const accountsKeys = Object.keys(accounts);
 
   const clickHandler = (username) => {
+    if (trx.injectInto)
+    {
+      trx.params[trx.injectInto] = username;
+    }
+    
     extension.runtime.sendMessage({
       action: MESSAGE_EVENTS.EXT_SIGN_AND_BROADCAST,
       data: {
